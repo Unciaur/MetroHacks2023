15f747f616e787cef994427011587db01fc38faf
Straw Biter <113861963+404jul@users.noreply.github.com>
2023-12-16 14:55:32 -0600

    gg

--- backend/static/scripts/audio.js
+++ backend/static/scripts/audio.js
@@ -1,10 +1,10 @@
 
-
+var recording = false;
 
 const audioCtx = new AudioContext();
 
-if (navigator.mediaDevices){
-    navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
+if (navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
         const mic = audioCtx.createMediaStreamSource(stream);
         console.log(mic);
     }).catch(err => {
@@ -13,8 +13,14 @@ if (navigator.mediaDevices){
 }
 
 
-function record(){
-    const mediaRecorder = new MediaRecorder(stream);
-    let chunks=[];
-    mediaRecorder.start();
+function toggleRecord() {
+    if (recording) {
+
+    } else {
+        const mediaRecorder = new MediaRecorder(stream);
+        let chunks = [];
+        mediaRecorder.start();
+    }
+    recording = !recording;
 }
+
