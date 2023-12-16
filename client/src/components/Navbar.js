b59de005a4a7c5f5bcc980f3c2bb180196bdb031
Straw Biter <113861963+404jul@users.noreply.github.com>
2023-12-16 16:14:13 -0600

    a

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+
+export default function Navbar(){
+    return <div id="navbar">
+    <div><a class="title" href="/">Let's Sign!</a></div>
+    <div id="nav-links">
+        <a href="/interpreter">Speech to Sign</a>
+        <a href="/">Coming Soon</a>
+    </div>
+</div>
+}
