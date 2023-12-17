3305f9651e8560412d1b4b52c35549f02abec2ff
gg <gggg@dsfjklsjdfklsdjfklsdj.com>
2023-12-17 00:18:50 +0000

    a

--- client/src/components/Mic.js
+++ client/src/components/Mic.js
@@ -1,25 +1,53 @@
-import {Microphone} from 'microphone-js'
-import {useState} from 'react';
+import { useState, useRef } from 'react';
 
-export default function Mic(){
-    const mic = Microphone();
-    const [recording, setRecording] = useState(false);
-    const toggleRecording = () => {
-        if (!recording){
-            mic.start();
-        } else {
-            mic.stop();
-            const blob = mic.getBlob();
-            console.log(blob)
-            mic.download();
-        }
-        setRecording(!recording);
+export default function Mic() {
+  const [recording, setRecording] = useState(false);
+  const mediaRecorder = useRef(null);
+  const recordedChunks = useRef([]);
+
+  const startRecording = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        mediaRecorder.current = new MediaRecorder(stream);
+        mediaRecorder.current.ondataavailable = e => {
+          if (e.data.size > 0) {
+            recordedChunks.current.push(e.data);
+          }
+        };
+        mediaRecorder.current.start();
+      });
+  };
+
+  const stopRecording = () => {
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const blob = new Blob(recordedChunks.current, { type: 'audio/webm' });
+      console.log(blob)
+      recordedChunks.current = [];
+      // Send blob to backend
+      const formData = new FormData();
+      formData.append('audio', blob);
+      fetch('https://api.letssign.xyz/interpret', {
+        method: 'POST',
+        body: formData
+      });
+    };
+  };
+
+  const toggleRecording = () => {
+    if (!recording) {
+      startRecording();
+    } else {
+      stopRecording();
     }
+    setRecording(!recording);
+  };
 
-    return <div>
-            <button onClick={toggleRecording}>
-                {mic.recording ? 'Recording' : 'Not Recording'}
-            </button>
+  return (
+    <div>
+      <button onClick={toggleRecording}>
+        {recording ? 'Stop Recording' : 'Start Recording'}
+      </button>
     </div>
-    
+  );
 }
